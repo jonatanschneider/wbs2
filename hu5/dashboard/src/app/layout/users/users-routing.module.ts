9d85fc925a49b562b3a08e657cf4a6847f604138
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-07 21:10:17 +0200

    Add edit route

--- hu5/dashboard/src/app/layout/users/users-routing.module.ts
+++ hu5/dashboard/src/app/layout/users/users-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users.component';
 import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
     {
@@ -11,6 +12,10 @@ const routes: Routes = [
     {
         path: 'add',
         component: AddComponent
+    },
+    {
+        path: 'edit/:id',
+        component: EditComponent
     },
 ];
 
