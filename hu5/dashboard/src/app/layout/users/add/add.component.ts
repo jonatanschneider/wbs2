44786bdea1b6d9f91d3dd4f99001b09142a00449
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-07 21:09:18 +0200

    Implement Add User via Copy and Paste

--- hu5/dashboard/src/app/layout/users/add/add.component.ts
+++ hu5/dashboard/src/app/layout/users/add/add.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss']
 })
-export class AddComponent implements OnInit {
+export class AddComponent {
+    username: string;
+    vorname: string;
+    nachname: string;
+    password: string;
 
-  constructor() { }
+    constructor(private userService: UserService,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    add() {
+        if(!this.username)  {
+            // this.notificationService.danger("Username must be set");
+            return;
+        } else if(!this.password){
+            // this.notificationService.danger("Password must be set");
+            return;
+        }
+        this.userService.addUser(new User(this.username, "", this.vorname, this.nachname, this.password))
+            .subscribe(result => {
+                if(result) {
+                    // this.notificationService.success("User created.");
+                    this.clear();
+                    this.router.navigate(['/users']);
+                } else {
+                    // this.notificationService.danger("User could not be created.");
+                }
+            });
+    }
 
+    clear() {
+        this.username = '';
+        this.vorname = '';
+        this.nachname = '';
+        this.password = '';
+    }
 }
