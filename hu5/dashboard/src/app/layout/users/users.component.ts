1051f50b6b0d0c125e842f6e10ae4ee4d9ac3ab3
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-07 21:10:41 +0200

    Implement User List via Copy and Paste

--- hu5/dashboard/src/app/layout/users/users.component.ts
+++ hu5/dashboard/src/app/layout/users/users.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
+import { User } from './user';
+import { UserService } from './user.service';
+import { EditComponent } from './edit/edit.component';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-users',
@@ -6,7 +12,58 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-    constructor() {}
+    users: User[];
 
-    ngOnInit() {}
+    constructor(private modalService: NgbModal,
+                private userService: UserService,
+                private route: ActivatedRoute,
+                private location: Location) {
+    }
+
+    ngOnInit() {
+        this.userService.getUsers().subscribe(users => {
+            this.users = users;
+        });
+        this.route.params.subscribe((params: any) => {
+            if (params['id'] != undefined) {
+                this.userService.getUser(params['id']).subscribe(user => {
+                    if (user) {
+                        this.editUser(user);
+                    }
+                });
+            }
+        });
+    }
+
+    deleteUser(user: User) {
+        this.userService.delete(user).subscribe(result => {
+            if (result) {
+                // this.notificationService.success('User ' + user.username + ' successfully deleted');
+                this.users.splice(this.users.findIndex(listItem => user === listItem), 1);
+            } else {
+                // this.notificationService.danger('User ' + user.username + ' could not be deleted');
+            }
+        });
+    }
+
+    editUser(user: User) {
+        this.location.go('/users/' + user.id);
+        const modalReference = this.modalService.open(EditComponent);
+        modalReference.componentInstance.setUser(user);
+        modalReference.result
+            .then((user: User) => {
+                this.location.go('/users');
+                this.userService.updateUser(user).subscribe(success => {
+                    if (success) {
+                        // this.notificationService.success('User ' + user.username + ' successfully updated');
+                        this.users.splice(this.users.findIndex(listItem => user.id === listItem.id), 1, user);
+                    } else {
+                        // this.notificationService.danger('User ' + user.username + ' could not be updated');
+                    }
+                });
+            })
+            .catch(() => {
+                this.location.go('/users');
+            });
+    }
 }
