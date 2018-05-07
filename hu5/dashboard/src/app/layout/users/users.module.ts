9ff7d840ea28aea859cf37d152f6c99de0b6fe69
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-05-07 21:10:56 +0200

    Update Module

--- hu5/dashboard/src/app/layout/users/users.module.ts
+++ hu5/dashboard/src/app/layout/users/users.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalStack } from '@ng-bootstrap/ng-bootstrap/modal/modal-stack';
+import { UserService } from './user.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, UsersRoutingModule],
-    declarations: [UsersComponent, AddComponent]
+    imports: [CommonModule, FormsModule, UsersRoutingModule, NgbModule.forRoot()],
+    declarations: [UsersComponent, AddComponent, EditComponent],
+    providers: [NgbModal, NgbModalStack, UserService],
+    entryComponents: [EditComponent]
 })
 export class UsersModule {}
